4c2cf50b1951aa6ef20e505393754f3a03d59630
filipematos95 <fm1995@gmail.com>
2018-04-30 23:11:27 +0200

    Added data_sample.py

---
+++ data_sample.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import random 
+import numpy as np
+import sys
+
+filename = "training_set_VU_DM_2014.csv"
+
+n = sum(1 for line in open(filename)) - 1
+
+if len(sys.argv) > 1 
+	s = sys.argv[1]
+else: 
+	print "Please spicify the size of the sample you want"
+	print "Using default 1000"
+
+skip = sorted(random.sample(xrange(1,n+1),n-s))
+
+df = pd.read_csv(filename, skiprows=skip)
+
+df.to_csv("random_samples"+str(s)+".csv")
