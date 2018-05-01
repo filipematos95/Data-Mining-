0478506180d842ab03b2fa7bdf4f967548746393
filipematos95 <fm1995@gmail.com>
2018-05-01 20:37:04 +0200

    Corrected Missing Values for Location score 2

--- correct_missing_values.py
+++ correct_missing_values.py
@@ -9,10 +9,17 @@ df = pd.read_csv(filename)
 #plt.show()
 
 prop_country_id = np.unique(df['prop_country_id'])
-
+prop_location_score2_new = []
 for country in prop_country_id:
 	print "Country: " + str(country)
 	#print np.mean(df.loc[df['prop_country_id'] == country]['prop_location_score2'])
-	country_values = np.array(df['prop_location_score2'][df['prop_country_id'] == country])
+	country_values = np.array(df[df['prop_country_id'] == country]['prop_location_score2'])
+	
+	df[(df['prop_country_id'] == country) & (df['prop_location_score2'] == np.nan)] =  np.nanmean(country_values)
+	
+	print "Values: "+ str(np.nanmean(country_values))
+	
 	if  np.isnan(np.nanmean(country_values)): 
 		print country_values
+
+df.to_csv(filename[:-4]+'_'+'corrected_loc_score_2'+'.csv')
