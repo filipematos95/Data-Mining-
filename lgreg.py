d5270bbf20aaea0aecfba5f68b61fd70b60d1cd3
filipematos95 <fm1995@gmail.com>
2018-05-18 10:39:26 +0100

    Updated lgreg.py

--- lgreg.py
+++ lgreg.py
@@ -1,8 +1,10 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import Imputer
+from sklearn.metrics import confusion_matrix
 
 train_file = 'train_1000000_preprocessed.csv'
 test_file = 'test_1000000_preprocessed.csv'
@@ -18,7 +20,7 @@ NAN = train[train.isnull().any(axis=1)][['srch_id','clicked']]
 train_x = train.drop(['booked','clicked'],axis = 1)
 train_y = train['clicked']
 
-imp_nan = Imputer(missing_values='NaN', strategy='median', axis=0)
+imp_nan = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
 imp_nan.fit(train_x)
 train_x = imp_nan.transform(train_x)
 
@@ -27,17 +29,8 @@ test_y = test['clicked']
 
 test_x = imp_nan.transform(test_x)
 
-logisticRegr = LogisticRegression()
-logisticRegr.fit(train_x, train_y)
-pred = logisticRegr.predict(test_x)
-score = logisticRegr.score(test_x,test_y)
-prob = logisticRegr.predict_proba(test_x)
-
-print "Accuracy: " + str(score)
-
-result = test[['srch_id','prop_id','clicked','booked']]
-
-result = result.assign(prob = pd.Series(prob[:,1]) ,index = 'prob')
+plt.hist(train_y)
+plt.show()
 
 def compute(r):
     k = len(r)
@@ -66,6 +59,40 @@ def score(ex):
     #ex['score'] = ex.groupby('srch_id').apply(lambda x: ndcg_at_k(x.points.values))
     return ex
 
-temp = score(result).dropna()[['srch_id', 'score']]
-print "NDCG Score: " + str(temp['score'].mean()) 
+def Logistic_Regression(train_x,train_y,test_x,test_y,test):
+    logisticRegr = LogisticRegression()
+    logisticRegr.fit(train_x, train_y)
+    pred = logisticRegr.predict(test_x)
+    score = logisticRegr.score(test_x,test_y)
+    prob = logisticRegr.predict_proba(test_x)
 
+    print "LG -> Accuracy: " + str(score)
+    print confusion_matrix(test_y, pred)
+
+    result = test[['srch_id','prop_id','booked','clicked']]
+
+    result = result.assign(prob = pd.Series(prob[:,1]) ,index = 'prob')
+
+    return result
+
+def Random_Forest(train_x,train_y,test_x,test_y):
+    clf = RandomForestClassifier(n_estimators = 200, n_jobs = -1)
+    clf.fit(train_x, train_y)
+    pred = clf.predict(test_x)
+    score = clf.score(test_x,test_y)
+    prob = clf.predict_proba(test_x)
+
+    print "RF -> Accuracy: " + str(score)
+
+    result = test[['srch_id','prop_id','clicked','booked']]
+
+    result = result.assign(prob = pd.Series(prob[:,1]) ,index = 'prob')
+
+    return result
+
+result = Logistic_Regression(train_x,train_y,test_x,test_y,test)
+temp = score(result).dropna()[['srch_id', 'score']]
+print "LG -> NDCG Score: " + str(temp['score'].mean()) 
+result = Random_Forest(train_x,train_y,test_x,test_y)
+temp = score(result).dropna()[['srch_id', 'score']]
+print "RF -> NDCG Score: " + str(temp['score'].mean()) 
